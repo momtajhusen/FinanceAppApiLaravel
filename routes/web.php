cf9569341713f9fb9dafdc7463b8936043c35405
momtajhusen <mumtaz666raza@gmail.com>
2024-09-14 23:47:17 +0530

    storage link route create in web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/command/storage', function () {
+    Artisan::call('storage:link');
+  });
